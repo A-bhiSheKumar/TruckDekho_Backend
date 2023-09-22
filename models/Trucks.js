4f1dbd7831e8ab7c8b789ec36f0b64f247a1a58f
Sidhant Dhyani <siddhantydhyani99@gmail.com>
2023-09-22 20:19:21 +0530

    Added Truck Model and removed demo data from commiting

---
+++ models/Trucks.js
@@ -0,0 +1,29 @@
+
+const mongoose = require('mongoose');
+
+const TrucksSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: true,
+        },
+        company:{
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        year: {
+            type: Number,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        }
+});
+
+const TrucksModel = mongoose.model('Truck', TrucksSchema);
+
+module.exports = TrucksModel;
