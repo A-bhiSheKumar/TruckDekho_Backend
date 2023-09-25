e43f4c0c8a4e351d664f6f7fcf0c11fd1a1afe84
Sidhant Dhyani <siddhantydhyani99@gmail.com>
2023-09-25 12:49:56 +0530

    Everything completed till filter feature.

--- models/Users.js
+++ models/Users.js
@@ -1,23 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
-    {
-        username:{
-            type: String,
-            required: true,
-            unique: true,
-        },
-        email:{
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password:{
-            type: String,
-            required: true,
-        }
-    }
-);
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-const UserModel= mongoose.model('User', UserSchema);
+const UserModel = mongoose.model("User", UserSchema);
 module.exports = UserModel;
